9a3eabbbead85b92714e916d33306359cd22adf9
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-11-08 01:53:38 -0300

    minor fixes

--- src/main/kotlin/com/sbarrasa/bank/product/factory/ProductTypeNotRegistered.kt
+++ src/main/kotlin/com/sbarrasa/bank/product/factory/ProductTypeNotRegistered.kt
@@ -1,5 +1,5 @@
 package com.sbarrasa.bank.product.factory
 
-class ProductTypeNotRegistered(productType: String) : RuntimeException("Producto no registrado ${productType}") {
+class ProductTypeNotRegistered(productType: String)
+   : RuntimeException("Producto no registrado ${productType}")
 
-}
